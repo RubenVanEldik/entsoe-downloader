7b7e64010556fefe8b52423e8f4b1b13ec169f91
Ruben van Eldik <r.h.vaneldik@students.uu.nl>
2022-12-24 14:49:05 +0100

    Refactor everything so the query options can be defined with a list

--- main.py
+++ main.py
@@ -1,44 +1,125 @@
+import datetime
 from dotenv import load_dotenv
 from entsoe import EntsoePandasClient
 from entsoe import exceptions
+from entsoe import mappings
 from os import getenv
 import pandas as pd
 import streamlit as st
 import xmltodict
 
-import interface
+QUERY_OPTIONS = {
+    "query_activated_balancing_energy": ["date_range", "country", "business_type"],
+    "query_aggregate_water_reservoirs_and_hydro_storage": ["date_range", "country"],
+    "query_contracted_reserve_amount": ["date_range", "country", "psr_type", "type_marketagreement_type"],
+    "query_contracted_reserve_prices": ["date_range", "country", "psr_type", "type_marketagreement_type"],
+    "query_crossborder_flows": ["date_range", "countries"],
+    "query_day_ahead_prices": ["date_range", "country"],
+    "query_generation": ["date_range", "country", "psr_type"],
+    "query_generation_forecast": ["date_range", "country"],
+    "query_generation_import": ["date_range", "country"],
+    "query_generation_per_plant": ["date_range", "country", "psr_type"],
+    "query_imbalance_prices": ["date_range", "country", "psr_type"],
+    "query_imbalance_volumes": ["date_range", "country"],
+    "query_import": ["date_range", "country"],
+    "query_installed_generation_capacity": ["date_range", "country", "psr_type"],
+    "query_installed_generation_capacity_per_unit": ["date_range", "country", "psr_type"],
+    "query_intraday_offered_capacity": ["date_range", "countries", "implicit"],
+    "query_load": ["date_range", "country"],
+    "query_load_and_forecast": ["date_range", "country"],
+    "query_load_forecast": ["date_range", "country"],
+    "query_net_position": ["date_range", "country"],
+    "query_net_transfer_capacity_dayahead": ["date_range", "countries"],
+    "query_net_transfer_capacity_monthahead": ["date_range", "countries", "dayahead"],
+    "query_net_transfer_capacity_weekahead": ["date_range", "countries"],
+    "query_net_transfer_capacity_yearahead": ["date_range", "countries"],
+    "query_offered_capacity": ["date_range", "countries", "contract_marketagreement_type"],
+    "query_procured_balancing_capacity": ["date_range", "countries"],
+    "query_scheduled_exchanges": ["date_range", "countries", "dayahead"],
+    "query_unavailability_of_generation_units": ["date_range", "countries"],
+    "query_unavailability_of_production_units": ["date_range", "country"],
+    "query_unavailability_transmission": ["date_range", "countries"],
+    "query_wind_and_solar_forecast": ["date_range", "country", "psr_type"],
+    "query_withdrawn_unavailability_of_generation_units": ["date_range", "country"],
+}
 
-# Set the page config
-st.set_page_config(page_title="ENTSO-E Downloader", page_icon="⚡")
 
-# Initialize the ENTSOE Pandas client
-load_dotenv(".env")
-if not getenv("ENTSOE_KEY"):
-    raise KeyError("ENTSOE_KEY variable has not been set")
-client = EntsoePandasClient(api_key=getenv("ENTSOE_KEY"))
-
-# Create the sidebar title
-st.sidebar.title("ENTSO-E Importer")
-
-# Select a ENTSO-E method
-entsoe_methods = [method for method in dir(client) if method.startswith("query_")]
-load_and_forecast_index = entsoe_methods.index("query_load_and_forecast")
-format_method_name = lambda method_name: method_name.replace("query_", "").replace("_", " ").capitalize()
-selected_method = st.sidebar.selectbox("Data type", entsoe_methods, index=load_and_forecast_index, format_func=format_method_name)
-
-# Create the header for the selected method
-st.header(format_method_name(selected_method))
-
-
-def retrieve(method, *args, **kwargs):
+def run():
     """
-    Fetch the data from the ENTSO-E API
+    Run the whole program
     """
+
+    # Set the page config
+    st.set_page_config(page_title="ENTSO-E Downloader", page_icon="⚡")
+
+    # Initialize the ENTSOE Pandas client
+    load_dotenv(".env")
+    if not getenv("ENTSOE_KEY"):
+        raise KeyError("ENTSOE_KEY variable has not been set")
+    client = EntsoePandasClient(api_key=getenv("ENTSOE_KEY"))
+
+    # Create the sidebar title
+    st.sidebar.title("ENTSO-E Importer")
+
+    # Select an ENTSO-E query
+    entsoe_queries = [query for query in dir(client) if query.startswith("query_")]
+    load_and_forecast_index = entsoe_queries.index("query_load_and_forecast")
+    format_query_name = lambda query_name: query_name.replace("query_", "").replace("_", " ").capitalize()
+    selected_query = st.sidebar.selectbox("Data type", entsoe_queries, index=load_and_forecast_index, format_func=format_query_name)
+
+    # Create the header for the selected query
+    st.header(format_query_name(selected_query))
+
+    # Get the options of the selected query and show a warning message if the options are not defined
+    selected_query_options = QUERY_OPTIONS.get(selected_query)
+    if selected_query_options is None:
+        st.warning("This query has not been implemented yet")
+        return
+
+    # Create the form
+    form = st.sidebar.form("inputs")
+    form.header("Options")
+
+    # Add the relevant parameters
+    args = []
+    kwargs = {}
+    if "date_range" in selected_query_options:
+        today = datetime.datetime.now()
+        yesterday = today - datetime.timedelta(days=1)
+        start, end = pd.to_datetime(form.date_input("Date range", value=(yesterday, today)), utc=True)
+        kwargs["start"] = start
+        kwargs["end"] = end
+    if "country" in selected_query_options:
+        areas = [code for code in dir(mappings.Area) if not code.startswith("_")]
+        country_code = form.selectbox("Country", areas, index=areas.index("NL"), format_func=lambda key: mappings.Area[key].meaning)
+        args.append(country_code)
+    if "countries" in selected_query_options:
+        areas = [code for code in dir(mappings.Area) if not code.startswith("_")]
+        col1, col2 = form.columns(2)
+        country_code_from = col1.selectbox("From", areas, index=areas.index("NL"), format_func=lambda key: mappings.Area[key].meaning)
+        args.append(country_code_from)
+        areas = mappings.NEIGHBOURS[country_code_from]
+        country_code_to = col2.selectbox("To", areas, index=0, format_func=lambda key: mappings.Area[key].meaning)
+        args.append(country_code_to)
+    if "business_type" in selected_query_options:
+        kwargs["business_type"] = form.selectbox("Business type", mappings.BSNTYPE.keys(), format_func=lambda key: mappings.BSNTYPE[key])
+    if "type_marketagreement_type" in selected_query_options:
+        kwargs["type_marketagreement_type"] = form.selectbox("Market agreement type", mappings.MARKETAGREEMENTTYPE.keys(), format_func=lambda key: mappings.MARKETAGREEMENTTYPE[key])
+    if "contract_marketagreement_type" in selected_query_options:
+        kwargs["contract_marketagreement_type"] = form.selectbox("Contract market agreement type", mappings.MARKETAGREEMENTTYPE.keys(), format_func=lambda key: mappings.MARKETAGREEMENTTYPE[key])
+    if "psr_type" in selected_query_options:
+        kwargs["psr_type"] = form.selectbox("PSR-type", mappings.PSRTYPE_MAPPINGS.keys(), format_func=lambda key: mappings.PSRTYPE_MAPPINGS[key])
+    if "implicit" in selected_query_options:
+        kwargs["implicit"] = form.checkbox("Implicit")
+    if "dayahead" in selected_query_options:
+        kwargs["dayahead"] = form.checkbox("Dayahead")
+
+    # Retrieve the data when the button is clicked
     if form.form_submit_button("Fetch data", type="primary"):
         try:
             with st.spinner("Fetching data from ENTSO-E"):
                 # Retrieve the data
-                data = getattr(client, method)(*args, **kwargs)
+                data = getattr(client, selected_query)(*args, **kwargs)
 
                 # Show a preview of the data as a line chart
                 chart_data = data.copy()
@@ -47,7 +128,7 @@ def retrieve(method, *args, **kwargs):
                 st.line_chart(data=chart_data)
 
                 # Show a download button
-                st.download_button("Download as CSV", data.to_csv(), file_name=f"{method.replace('query_', '')}.csv", mime="text/csv")
+                st.download_button("Download as CSV", data.to_csv(), file_name=f"{selected_query.replace('query_', '')}.csv", mime="text/csv")
         except exceptions.NoMatchingDataError:
             st.error("There is no data available for this country in this time period")
             return
@@ -67,187 +148,7 @@ def retrieve(method, *args, **kwargs):
             elif hasattr(e, "response") and hasattr(e.response, "reason"):
                 st.error(e.response.reason)
             else:
-                st.error("Something went wrong...")
-            return
+                st.write(e)
 
 
-if selected_method == "query_activated_balancing_energy":
-    form = interface.form()
-    country = interface.country(form)
-    start, end = interface.date_range(form)
-    business_type = interface.business_type(form)
-    retrieve("query_activated_balancing_energy", country, start=start, end=end, business_type=business_type)
-
-elif selected_method == "query_contracted_reserve_amount":
-    form = interface.form()
-    start, end = interface.date_range(form)
-    country = interface.country(form)
-    psr_type = interface.psr_type(form)
-    type_marketagreement_type = interface.market_agreement_type(form)
-    retrieve("query_contracted_reserve_amount", country, start=start, end=end, type_marketagreement_type=type_marketagreement_type, psr_type=psr_type)
-
-elif selected_method == "query_day_ahead_prices":
-    form = interface.form()
-    start, end = interface.date_range(form)
-    country = interface.country(form)
-    retrieve("query_day_ahead_prices", country, start=start, end=end)
-
-elif selected_method == "query_net_position_dayahead":
-    form = interface.form()
-    start, end = interface.date_range(form)
-    country = interface.country(form)
-    retrieve("query_net_position_dayahead", country, start=start, end=end)
-
-elif selected_method == "query_crossborder_flows":
-    form = interface.form()
-    start, end = interface.date_range(form)
-    col1, col2 = form.columns(2)
-    country_from = interface.country(col1, label="From")
-    country_to = interface.country(col2, label="To", neighbouring_area=country_from)
-    retrieve("query_crossborder_flows", country_from, country_to, start=start, end=end)
-
-elif selected_method == "query_scheduled_exchanges":
-    form = interface.form()
-    start, end = interface.date_range(form)
-    col1, col2 = form.columns(2)
-    country_from = interface.country(col1, label="From")
-    country_to = interface.country(col2, label="To", neighbouring_area=country_from)
-    dayahead = form.checkbox("Dayahead")
-    retrieve("query_scheduled_exchanges", country_from, country_to, start=start, end=end, dayahead=dayahead)
-
-elif selected_method == "query_net_transfer_capacity_dayahead":
-    form = interface.form()
-    start, end = interface.date_range(form)
-    col1, col2 = form.columns(2)
-    country_from = interface.country(col1, label="From")
-    country_to = interface.country(col2, label="To", neighbouring_area=country_from)
-    retrieve("query_net_transfer_capacity_dayahead", country_from, country_to, start=start, end=end)
-
-elif selected_method == "query_net_transfer_capacity_weekahead":
-    form = interface.form()
-    start, end = interface.date_range(form)
-    col1, col2 = form.columns(2)
-    country_from = interface.country(col1, label="From")
-    country_to = interface.country(col2, label="To", neighbouring_area=country_from)
-    retrieve("query_net_transfer_capacity_weekahead", country_from, country_to, start=start, end=end)
-
-elif selected_method == "query_net_transfer_capacity_monthahead":
-    form = interface.form()
-    start, end = interface.date_range(form)
-    col1, col2 = form.columns(2)
-    country_from = interface.country(col1, label="From")
-    country_to = interface.country(col2, label="To", neighbouring_area=country_from)
-    dayahead = form.checkbox("Dayahead")
-    retrieve("query_net_transfer_capacity_monthahead", country_from, country_to, start=start, end=end)
-
-elif selected_method == "query_net_transfer_capacity_yearahead":
-    form = interface.form()
-    start, end = interface.date_range(form)
-    col1, col2 = form.columns(2)
-    country_from = interface.country(col1, label="From")
-    country_to = interface.country(col2, label="To", neighbouring_area=country_from)
-    retrieve("query_net_transfer_capacity_yearahead", country_from, country_to, start=start, end=end)
-
-elif selected_method == "query_intraday_offered_capacity":
-    form = interface.form()
-    start, end = interface.date_range(form)
-    col1, col2 = form.columns(2)
-    country_from = interface.country(col1, label="From")
-    country_to = interface.country(col2, label="To", neighbouring_area=country_from)
-    implicit = st.checkbox("Implicit")
-    retrieve("query_intraday_offered_capacity", country_from, country_to, start=start, end=end, implicit=implicit)
-
-elif selected_method == "query_load":
-    form = interface.form()
-    start, end = interface.date_range(form)
-    country = interface.country(form)
-    retrieve("query_load", country, start=start, end=end)
-
-elif selected_method == "query_load_forecast":
-    form = interface.form()
-    start, end = interface.date_range(form)
-    country = interface.country(form)
-    retrieve("query_load_forecast", country, start=start, end=end)
-
-elif selected_method == "query_load_and_forecast":
-    form = interface.form()
-    start, end = interface.date_range(form)
-    country = interface.country(form)
-    retrieve("query_load_and_forecast", country, start=start, end=end)
-
-elif selected_method == "query_generation_forecast":
-    form = interface.form()
-    start, end = interface.date_range(form)
-    country = interface.country(form)
-    retrieve("query_generation_forecast", country, start=start, end=end)
-
-elif selected_method == "query_wind_and_solar_forecast":
-    form = interface.form()
-    start, end = interface.date_range(form)
-    country = interface.country(form)
-    psr_type = interface.psr_type(form)
-    retrieve("query_wind_and_solar_forecast", country, start=start, end=end, psr_type=psr_type)
-
-elif selected_method == "query_generation":
-    form = interface.form()
-    start, end = interface.date_range(form)
-    country = interface.country(form)
-    psr_type = interface.psr_type(form)
-    retrieve("query_generation", country, start=start, end=end, psr_type=psr_type)
-
-elif selected_method == "query_generation_per_plant":
-    form = interface.form()
-    start, end = interface.date_range(form)
-    country = interface.country(form)
-    psr_type = interface.psr_type(form)
-    retrieve("query_generation_per_plant", country, start=start, end=end, psr_type=psr_type)
-
-elif selected_method == "query_installed_generation_capacity":
-    form = interface.form()
-    start, end = interface.date_range(form)
-    country = interface.country(form)
-    psr_type = interface.psr_type(form)
-    retrieve("query_installed_generation_capacity", country, start=start, end=end, psr_type=psr_type)
-
-elif selected_method == "query_installed_generation_capacity_per_unit":
-    form = interface.form()
-    start, end = interface.date_range(form)
-    country = interface.country(form)
-    psr_type = interface.psr_type(form)
-    retrieve("query_installed_generation_capacity_per_unit", country, start=start, end=end, psr_type=psr_type)
-
-elif selected_method == "query_imbalance_prices":
-    form = interface.form()
-    start, end = interface.date_range(form)
-    country = interface.country(form)
-    psr_type = interface.psr_type(form)
-    retrieve("query_imbalance_prices", country, start=start, end=end, psr_type=psr_type)
-
-elif selected_method == "query_contracted_reserve_prices":
-    form = interface.form()
-    start, end = interface.date_range(form)
-    country = interface.country(form)
-    psr_type = interface.psr_type(form)
-    type_marketagreement_type = interface.market_agreement_type(form)
-    retrieve("query_contracted_reserve_prices", country, start=start, end=end, type_marketagreement_type=type_marketagreement_type, psr_type=psr_type)
-
-elif selected_method == "query_withdrawn_unavailability_of_generation_units":
-    form = interface.form()
-    start, end = interface.date_range(form)
-    country = interface.country(form)
-    retrieve("query_withdrawn_unavailability_of_generation_units", country, start=start, end=end)
-
-elif selected_method == "query_import":
-    form = interface.form()
-    start, end = interface.date_range(form)
-    country = interface.country(form)
-    retrieve("query_import", country, start=start, end=end)
-
-elif selected_method == "query_generation_import":
-    form = interface.form()
-    start, end = interface.date_range(form)
-    country = interface.country(form)
-    retrieve("query_generation_import", country, start=start, end=end)
-
-else:
-    st.warning("This method has not been implemented yet")
+run()
